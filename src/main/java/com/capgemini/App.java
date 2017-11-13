5e6ad9096ed2a9ce88c91114efa98411ac9dddea
karth21 <karthikeyaneswaran21@gmail.com>
2017-11-13 12:17:34 +0530

    First Commit

---
+++ src/main/java/com/capgemini/App.java
@@ -0,0 +1,16 @@
+package com.capgemini;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int i;
+		try {
+			System.out.println("Hell is MyClass");
+		} catch (Exception e) {}
+    }
+}
